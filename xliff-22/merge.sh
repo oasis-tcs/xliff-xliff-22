645516a1cb98a1329708ee28832332471c3e4ed4
Mihai Nita <nmihai_2000@yahoo.com>
2025-11-17 10:04:57 -0800

    Fix issue 45: Bad example in pgs module (duplicate IDs)

--- xliff-22/merge.sh
+++ xliff-22/merge.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-rm -f xliff-extended-v2.2-csd03-part2.xml
-java -cp lib/merger.jar com.maxprograms.xml.Merger xliff22.xml xliff-extended-v2.2-part2.xml
+XML_FILE=xliff-extended-v2.2-part2.xml
+rm -f $XML_FILE
+java -cp lib/merger.jar com.maxprograms.xml.Merger xliff22.xml $XML_FILE
